7cbda87c3ee546cc3c29b735be6120866c270588
Lara Oliveira <lara.bia@outlook.com.br>
2020-06-25 16:51:16 -0300

    Added CSS

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -17,4 +17,84 @@ export default {
 
 <style lang="scss">
 @import '~bulma';
+
+body {
+  background-color: #FEAEA7;
+  color: #ffffff;
+  overflow: hidden;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Titan+One&display=swap');
+
+.container {
+  padding-bottom: 0.75em;
+}
+//alt shift A
+#welcome-text {
+  font-family: 'Titan One', cursive;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 60px;
+  line-height: 69px;
+  color:#ffffff;
+}
+
+.hello-container {
+  position: relative;
+}
+
+.hello-title {
+  position: relative;
+  z-index: 2;
+}
+
+#circle-image {
+  position: absolute;
+  height: 150px;
+  width: 115px;
+  left: calc(50% - 175px);
+  top: calc(75% - 135px);
+}
+
+.title {
+  font-family: 'Titan One', cursive;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 34px;
+  color: #ffffff;
+  padding: 0 0 0.30em 0;
+  margin: 0 0 0 0 !important;
+  z-index: 1;
+}
+
+.subtitle {
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+  padding: 0 0.10em 0.60em 0.10em;
+  margin: 0 0 0 0 !important;
+}
+
+a {
+  color: #ffffff;
+}
+
+.icon-image {
+  margin: 0.4em 1.2em 0 1.0em;
+  padding: 0.60em 0 0.60em 0;
+}
+
+#social-medias {
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 25px;
+  line-height: 23px;
+  padding: 0.60em 0 0.60em 0;
+}
+
+.footer {
+  padding: 0.60em 0 0.60em 0;
+  background-color: #FEAEA7;
+}
 </style>
--- vue-app/src/components/Portifolio.vue
+++ vue-app/src/components/Portifolio.vue
@@ -1,75 +1,89 @@
 <template>
   <div class="Portifolio">
     <header>
-      <h1 class="title">
-        Welcome
-      </h1>
+      <section class="hero is-fullheight">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title" id="welcome-text">Welcome</h1>
+          </div>
+        </div>
+      </section>
     </header>
-    <article>
-      <header>
-        <div>
-          <h2 class="title">
-            Hi. I’m Lara 
-          </h2>
-          <h3 class="subtitle">
-            I’m a System Development student currently living in Joinville, Brazil. I have a passion for front-end development, UI and UX.
-          </h3>
-        </div>
-        <div>
-          <h2 class="title">
-            Projects
-          </h2>
-          <h3 class="subtitle">
-            <a 
-              href="https://cricketgate.web.app/login" 
-              target="_blank"
-            >
-              CricketGate
+    <article class="has-text-centered">
+      <div class="columns is-inline-block-mobile is-centered">
+        <div class="column">
+          <div class="container">
+            <header>
+              <div class="hello-container">
+                <img
+                  id="circle-image"
+                  class="icon-image"
+                  src="../images/Circle-image.svg"
+                  alt="Figma icon"
+                />
+                <h2 class="title hello-title">Hi. I’m Lara</h2>
+              </div>
+            </header>
+            <h3
+              class="subtitle"
+            >I’m a System Development student currently living in Joinville, Brazil. I have a passion for front-end development, UI and UX.</h3>
+          </div>
+          <div class="container">
+            <h2 class="title">Projects</h2>
+            <h3 class="subtitle">
+              <a href="https://cricketgate.web.app/login" target="_blank">CricketGate</a>
+            </h3>
+            <h3 class="subtitle">
+              <a
+                href="https://medium.com/@larabeatrizdeoliveira/estudo-de-caso-ui-ux-trazer-uma-nova-experi%C3%AAncia-para-os-usu%C3%A1rios-da-the-body-shop-por-meio-do-b9d379e47650"
+                target="_blank"
+              >Study case</a>
+            </h3>
+          </div>
+          <div class="container">
+            <h2 class="title" id="tecnologies-text">Tecnologies</h2>
+            <img class="icon-image" src="../images/JS-icon.svg" alt="Javascript icon" />
+            <img class="icon-image" src="../images/Vue-icon.svg" alt="Vue icon" />
+            <img class="icon-image" src="../images/CSS-icon.svg" alt="CSS icon" />
+            <img class="icon-image" src="../images/HTML-icon.svg" alt="HTML icon" />
+            <img class="icon-image" src="../images/Figma-icon.svg" alt="Figma icon" />
+          </div>
+          <div class="container">
+            <h2 class="title" id="social-medias">Let’s be internet BFFs</h2>
+            <a href="https://github.com/lrbtz" target="_blank">
+              <img
+                class="icon-image"
+                src="../images/Github-icon.svg"
+                alt="lcon to access my github page"
+              />
             </a>
-          </h3>
-          <h3 class="subtitle">
-            <a
-            href="https://medium.com/@larabeatrizdeoliveira/estudo-de-caso-ui-ux-trazer-uma-nova-experi%C3%AAncia-para-os-usu%C3%A1rios-da-the-body-shop-por-meio-do-b9d379e47650"
-            target="_blank"
-            >
-              Study case
+            <a href="https://www.linkedin.com/in/lara-beatriz-de-oliveira/" target="_blank">
+              <img
+                class="icon-image"
+                src="../images/Linkedin-icon.svg"
+                alt="lcon to access my linkedin page"
+              />
             </a>
-          </h3>
+            <a href="https://medium.com/@larabeatrizdeoliveira" target="_blank">
+              <img
+                class="icon-image"
+                src="../images/Medium-icon.svg"
+                alt="lcon to access my medium page"
+              />
+            </a>
+            <a href="mailto:lara.bia@outlook.com.br" target="_blank">
+              <img class="icon-image" src="../images/Email-icon.svg" alt="lcon to send me an email" />
+            </a>
+          </div>
         </div>
-        <div>
-          <h2 class="title">
-            Tecnologies
-          </h2>
-            <img src="../images/JS-icon.svg" alt="Javascript icon">
-            <img src="../images/Vue-icon.svg" alt="Vue icon">
-            <img src="../images/CSS-icon.svg" alt="CSS icon">
-            <img src="../images/HTML-icon.svg" alt="HTML icon">
-            <img src="../images/Figma-icon.svg" alt="Figma icon">
-        </div>
-        <div>
-          <h2 class="title">
-            Let’s be internet BFFs
-          </h2>
-          <a href="https://github.com/lrbtz" target="_blank"><img src="../images/Github-icon.svg" alt="lcon to access my github page"></a>
-          <a href="https://www.linkedin.com/in/lara-beatriz-de-oliveira/" target="_blank"><img src="../images/Linkedin-icon.svg" alt="lcon to access my linkedin page"></a>
-          <a href="https://medium.com/@larabeatrizdeoliveira" target="_blank"><img src="../images/Medium-icon.svg" alt="lcon to access my medium page"></a>
-          <a href="mailto:lara.bia@outlook.com.br" target="_blank"><img src="../images/Email-icon.svg" alt="lcon to send me an email"></a>
-        </div>
-      </header>
+      </div>
     </article>
-    <footer>
-      Lara Oliveira. All Rights Reserved.
-    </footer>
+    <footer class="footer has-text-centered">Lara Oliveira. All Rights Reserved.</footer>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Portifolio',
-}
+  name: "Portifolio"
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
