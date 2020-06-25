729f5c934f828b8f8fb69370632c7459f1553a2c
Lara Oliveira <lara.bia@outlook.com.br>
2020-06-25 01:31:44 -0300

    HTML content

---
+++ vue-app/src/components/Portifolio.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="Portifolio">
+    <header>
+      <h1 class="title">
+        Welcome
+      </h1>
+    </header>
+    <article>
+      <header>
+        <div>
+          <h2 class="title">
+            Hi. I’m Lara 
+          </h2>
+          <h3 class="subtitle">
+            I’m a System Development student currently living in Joinville, Brazil. I have a passion for front-end development, UI and UX.
+          </h3>
+        </div>
+        <div>
+          <h2 class="title">
+            Projects
+          </h2>
+          <h3 class="subtitle">
+            <a 
+              href="https://cricketgate.web.app/login" 
+              target="_blank"
+            >
+              CricketGate
+            </a>
+          </h3>
+          <h3 class="subtitle">
+            <a
+            href="https://medium.com/@larabeatrizdeoliveira/estudo-de-caso-ui-ux-trazer-uma-nova-experi%C3%AAncia-para-os-usu%C3%A1rios-da-the-body-shop-por-meio-do-b9d379e47650"
+            target="_blank"
+            >
+              Study case
+            </a>
+          </h3>
+        </div>
+        <div>
+          <h2 class="title">
+            Tecnologies
+          </h2>
+            <img src="" alt="Javascript icon">
+            <img src="" alt="Vue icon">
+            <img src="" alt="CSS icon">
+            <img src="" alt="HTML icon">
+            <img src="" alt="Figma icon">
+        </div>
+        <div>
+          <h2 class="title">
+            Let’s be internet BFFs
+          </h2>
+          <a href="https://github.com/lrbtz" target="_blank"></a>
+          <a href="https://www.linkedin.com/in/lara-beatriz-de-oliveira/" target="_blank"></a>
+          <a href="https://medium.com/@larabeatrizdeoliveira" target="_blank"></a>
+          <a href="mailto:lara.bia@outlook.com.br" target="_blank"></a>
+        </div>
+      </header>
+    </article>
+    <footer>
+      Lara Oliveira. All Rights Reserved.
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Portifolio',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
